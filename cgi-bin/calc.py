f408f9fd8af1419bb8ff202727db189ad725a4fd
Anastasiia-Ni <stasy247@mail.ru>
2022-12-29 09:33:57 -0800

    calc.py fix

--- cgi-bin/calc.py
+++ cgi-bin/calc.py
@@ -11,44 +11,44 @@ arg1 = form.getvalue('f_num')
 oper = form.getvalue('oper')
 arg3 = form.getvalue('s_num')
 
-print(os.environ["QUERY_STRING"], file=sys.stderr)
+# print(os.environ["QUERY_STRING"], file=sys.stderr)
 print("HTTP/1.1 200 OK")
 print("Content-type: text/html\r\n\r\n")
 
 if not oper or not arg1 or not arg3:
 	print("Parameters are not correct")
-	exit(1)
+	exit()
 
 if len(oper) != 1:
     print("Parameters are not correct")
-    exit(1)
+    exit()
 
 if ord(oper) == 32:
 	oper = '+'
 
 if oper not in "+-/*^r":
     print("Parameters are not correct")
-    exit(1)
+    exit()
 if arg1[0] in '-+':
     if not arg1[1:].isdigit():
         print("Parameters are not correct")
-        exit(1)
+        exit()
 elif not arg1.isdigit():
         print("Parameters are not correct")
-        exit(1)
+        exit()
 if arg3[0] in '-+':
     if not arg3[1:].isdigit():
         print("Parameters are not correct")
-        exit(1)
+        exit()
 elif not arg3.isdigit():
         print("Parameters are not correct")
-        exit(1)
+        exit()
 
 num1 = int(arg1)
 num2 = int(arg3)
 if (num1 > 10000 or num2 > 10000):
     print("Enter a lower number")
-    exit(1)
+    exit()
 result = 0
 
 if oper == '+': result = num1 + num2
